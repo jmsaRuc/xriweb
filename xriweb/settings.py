d97c73bfc6ce801df771699b84788bbef7e33044
jmsaRuc <jmsa@ruc.dk>
2023-10-14 07:05:01 +0200

    update

--- xriweb/settings.py
+++ xriweb/settings.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     with environment variables.
     """
 
-    host: str = "127.0.0.1"
+    host: str = "localhost"
     port: int = 8000
     # quantity of workers for uvicorn
     workers_count: int = 1
